c6329c808c698a376a43d22969f75b40529f6b92
WilliamBostrom <133894488+WilliamBostrom@users.noreply.github.com>
2024-04-22 16:03:26 +0200

    useReducer

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,43 @@
+import { useEffect, useReducer } from "react";
+import Header from "./Header";
+import Mains from "./Mains";
+import Loader from "./Loader";
+import Error from "./Error";
+
+const inititalState = {
+  questions: [],
+  //loading, error, ready, active, finished
+  status: "loading",
+};
+
+function reducer(state, action) {
+  switch (action.type) {
+    case "dataReceived":
+      return { ...state, question: action.payload, status: "ready" };
+    case "dataFailed":
+      return { ...state, status: "error" };
+    default:
+      throw new Error("Unknown");
+  }
+}
+
 function App() {
-  return <>Hej</>;
+  const [{ question, status }, dispatch] = useReducer(reducer, inititalState);
+  useEffect(() => {
+    fetch("http://localhost:8000/questions")
+      .then((res) => res.json())
+      .then((data) => dispatch({ type: "dataReceived", payload: data }))
+      .catch((err) => dispatch({ type: "dataFailed" }));
+  }, []);
+  return (
+    <div className="app">
+      <Header />
+      <Mains>
+        {status === "loading" && <Loader />}
+        {status === "error" && <Error />}
+      </Mains>
+    </div>
+  );
 }
 
 export default App;
